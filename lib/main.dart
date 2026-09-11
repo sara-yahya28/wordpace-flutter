6dbdf7b99a335dca7b5e024ef762d0b3ecd56ac7
Sara <sara.yahyamqbl@gmail.com>
2026-09-11 21:01:37 +0300

    feat: add main entry point updates

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,7 @@ class MyApp extends StatelessWidget {
           '/login': (context) => const LoginScreen(),
           '/register': (context) => const RegisterScreen(),
           '/home': (context) => const MainLayoutScreen(),
+          '/welcome':(context)=>const WelcomeScreen(),
         },
       ),
     );
