7e6d29e399476d3ac9f64ae61bb9fccdee0cdf7f
Sara <sara.yahyamqbl@gmail.com>
2026-09-22 14:02:51 +0300

    feat: implement remote data source for likes and add saveAllFavoritePosts method

--- lib/features/likes/data/datasources/like_local_data_source.dart
+++ lib/features/likes/data/datasources/like_local_data_source.dart
@@ -7,30 +7,33 @@ import 'dart:convert';
 abstract class LikeLocalDataSource {
   Future<List<PostModel>> getFavoritePosts();
   Future<void> saveFavoritePost(PostModel post);
+  Future<void> saveAllFavoritePosts(List<PostModel> posts);
   Future<bool> isFavorite(int postId);
   Future<void> removeFavoritePost(int postId);
 }
 
 class LikeLocalDataSourceImpl implements LikeLocalDataSource {
   final CacheHelper cache;
-  final UserLocalDataSource userLocalDataSource; // Changed key to be more specific
-  LikeLocalDataSourceImpl({required this.cache, required this.userLocalDataSource});
+  final UserLocalDataSource userLocalDataSource;
 
+  LikeLocalDataSourceImpl({
+    required this.cache,
+    required this.userLocalDataSource,
+  });
 
-// gets key fot each user
-Future<String?> _getKey() async {
-  try {
-    final user = await userLocalDataSource.getLastUser();
-    return "CachedFavoritePosts_${user.id}";
-  } on CacheException {
-    return null;
+  Future<String?> _getKey() async {
+    try {
+      final user = await userLocalDataSource.getLastUser();
+      return "CachedFavoritePosts_${user.id}";
+    } on CacheException {
+      return null;
+    }
   }
-}
 
   @override
   Future<List<PostModel>> getFavoritePosts() async {
-    final key=await _getKey();
-    if (key==null) return [];
+    final key = await _getKey();
+    if (key == null) return [];
 
     final jsonString = cache.getDataString(key: key);
     if (jsonString != null) {
@@ -46,12 +49,12 @@ Future<String?> _getKey() async {
 
   @override
   Future<void> saveFavoritePost(PostModel post) async {
-        final key=await _getKey();
-    if (key==null) return;
-    
+    final key = await _getKey();
+    if (key == null) return;
+
     final posts = await getFavoritePosts();
-    final exist = posts.any((p) => p.id == post.id);
-    if (!exist) {
+    final exists = posts.any((p) => p.id == post.id);
+    if (!exists) {
       posts.add(post);
       final String encodedData =
           json.encode(posts.map((p) => p.toJson()).toList());
@@ -60,21 +63,30 @@ Future<String?> _getKey() async {
   }
 
   @override
+  Future<void> saveAllFavoritePosts(List<PostModel> posts) async {
+    final key = await _getKey();
+    if (key == null) return;
+
+    final String encodedData =
+        json.encode(posts.map((p) => p.toJson()).toList());
+    await cache.saveData(key: key, value: encodedData);
+  }
+
+  @override
   Future<bool> isFavorite(int postId) async {
-    final post = await getFavoritePosts();
-    final isLiked = post.any((p) => p.id == postId);
-    return isLiked;
+    final posts = await getFavoritePosts();
+    return posts.any((p) => p.id == postId);
   }
 
   @override
   Future<void> removeFavoritePost(int postId) async {
-        final key=await _getKey();
-    if (key==null) return ;
+    final key = await _getKey();
+    if (key == null) return;
 
-    final post = await getFavoritePosts();
-    post.removeWhere((p) => p.id == postId);
+    final posts = await getFavoritePosts();
+    posts.removeWhere((p) => p.id == postId);
     final String encodedData =
-        json.encode(post.map((p) => p.toJson()).toList());
+        json.encode(posts.map((p) => p.toJson()).toList());
     await cache.saveData(key: key, value: encodedData);
   }
 }
